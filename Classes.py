036bd27114722614aaead5171371f20e2871c141
MR-Raven <pavel.fakanov@gmail.com>
2018-03-26 20:52:22 +0300

    Add word fixing

--- Classes.py
+++ Classes.py
@@ -54,6 +54,8 @@ class BoardWord:
         return self.hash in hashesAI[self.dictType].keys() and self.string in hashesAI[self.dictType][self.hash]
 
 
+
+
 class Cell:
     def __init__(self, row, col):
         from config import bonuses
@@ -99,12 +101,12 @@ class Board:
             colEnd = word.cells[len(word.cells) - 1].col
             if rowBegin == rowEnd:  # "Horizontal orientation"
                 counter = 0
-                for letter in word:
+                for letter in word.string:
                     self.board[rowBegin][colBegin + counter].letter = letter
 
             elif colBegin == colEnd:  # Vertical orientation
                 counter = 0
-                for letter in word:
+                for letter in word.string:
                     self.board[rowBegin + counter][colBegin].letter = letter
 
     def printBoard(self):
