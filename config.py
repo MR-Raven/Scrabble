1ac11be1d3992a549434d9b247ea6115a3aa8647
MR-Raven <pavel.fakanov@gmail.com>
2018-03-26 13:26:16 +0300

    Change path

--- config.py
+++ config.py
@@ -1,26 +1,12 @@
 from hashing import *
 from Classes import *
 
-typeData = ["Basic", "Big", "Large", "Medium", "Small"]
-forbidden = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", ".", "/", "-", "&", "'"]
-###
-hashesAI = dict()
-for typeAI in typeData:
-    hashesAI[typeAI] = hashTabling(typeAI)
-#print(hashesAI) Working though
-
-bonuses = setBonuses()
-scores = {' ': 0, 'a': 1, 'b': 2, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2,
-              'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1,
-              'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4,
-              'x': 8, 'y': 4, 'z': 10}
-
-
 def setBonuses():
-    global bonuses
+    bonuses = {}
     for x in range(0, 15):
         for y in range(0, 15):
-            bonuses[Coordinate(x, y)] = "00"
+            point = Coordinate(x, y)
+            bonuses[point] = "00"
 
     for x in range(0, 15, 7):  ### Setting triple word score cells
         for y in range(0, 15, 7):
@@ -45,3 +31,20 @@ def setBonuses():
         bonuses[Coordinate(x, 14 - y)] = "2L"
         bonuses[Coordinate(14 - x, y)] = "2L"
         bonuses[Coordinate(14 - x, 14 - y)] = "2L"
+    return bonuses
+
+typeData = ["Basic", "Big", "Large", "Medium", "Small"]
+forbidden = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", ".", "/", "-", "&", "'"]
+###
+hashesAI = dict()
+for typeAI in typeData:
+    hashesAI[typeAI] = hashTabling(typeAI)
+#print(hashesAI) Working though
+
+bonuses = setBonuses()
+scores = {' ': 0, 'a': 1, 'b': 2, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2,
+              'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1,
+              'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4,
+              'x': 8, 'y': 4, 'z': 10}
+
+
--- wordSort.py
+++ wordSort.py
@@ -14,11 +14,13 @@ def forbiddenCheck(curWord):
     return flag
 
 def sorting(wordDataUnsorted, type):
-    pathSorted = "/home/pavlik/Pavlik/python/Scrabble/dicts/wordsSorted" + type + ".txt"
-    # CHANGE PATH IN MY COLLEAGUES COMPUTER
+    firstWordsPath = "/home/pavlik/Pavlik/python/Scrabble/dicts/wordsSorted"  # The first path is for Pavlik, the second one is for Pavel
+    secondWordsPath = "/home/pavel/MyDocs/Programming/Python/Scrabble/dicts/wordsSorted"
+    firstHashesPath = "/home/pavlik/Pavlik/python/Scrabble/hashes/hashes"
+    secondHashesPath = "/home/pavel/MyDocs/Programming/Python/Scrabble/hashes/hashes"
+    pathSorted = firstWordsPath + type + ".txt"
     wordDataSorted = open(pathSorted, "w")
-    pathHashes = "/home/pavlik/Pavlik/python/Scrabble/hashes/hashes" + type + ".txt"
-    # CHANGE PATH IN MY COLLEAGUES COMPUTER
+    pathHashes = firstHashesPath + type + ".txt"
     hashesData = open(pathHashes, "w")
     curWord = wordDataUnsorted.readline()
 
@@ -37,7 +39,9 @@ def sorting(wordDataUnsorted, type):
 
 # Full-sized Sort + Hashingas
 for type in typeData:
-    path = "/home/pavlik/Pavlik/python/Scrabble/dicts/wordsUnsorted" + type + ".txt"
+    firstUnsortedPath = "/home/pavlik/Pavlik/python/Scrabble/dicts/wordsUnsorted"  # The first path is for Pavlik, the second one is for Pavel
+    secondUnsortedPath = "/home/pavel/MyDocs/Programming/Python/Scrabble/dicts/wordsUnsorted"
+    path = firstUnsortedPath + type + ".txt"
     # CHANGE PATH IN MY COLLEAGUES COMPUTER
     wordDataUnsorted = open(path, "r")
     sorting(wordDataUnsorted, type)
